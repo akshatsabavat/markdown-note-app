48aeba681c564abd14938dba9beab57e3e283da7
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-03-10 16:42:04 +0530

    setup compelete

---
+++ note-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading, Container } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Container maxWidth="1000px">
+      <Heading>Hi</Heading>
+    </Container>
+  );
+};
+
+export default App;
