f1f347ce868d2d313fd823b4de8e1b1c8864a314
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-03-10 17:41:21 +0530

    routing added

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -1,9 +1,18 @@
 import { Heading, Container } from "@chakra-ui/react";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
-    <Container maxWidth="1000px">
-      <Heading>Hi</Heading>
+    <Container maxWidth="1000px" marginY="3rem">
+      <Routes>
+        <Route path="/" element={<h1>HomePage</h1>} />
+        <Route path="/new" element={<h1>NewNotePage</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Container>
   );
 };
